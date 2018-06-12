2df15c1aa9f4067057ddbfc34e3cf22e793d6a3c
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-06-12 19:51:57 +0300

    Add Ride My Way APP

---
+++ javascript/myscripts.js
@@ -0,0 +1,11 @@
+
+function mySelect() {
+    document.getElementById("driver").select();
+}
+function signup(){
+    document.getElementById('newuser').style.display='block';
+}
+
+    function select(){
+        window.location='option.html';
+    }
