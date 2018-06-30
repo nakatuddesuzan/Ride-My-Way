08a903ed092aaf8c069b77ef1690a080b4c6c41e
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-06-30 12:09:15 +0300

    Add changes

--- app/api/offers/__init__.py
+++ app/api/offers/__init__.py
@@ -3,8 +3,8 @@ from flask_restful import Api
 from .views import AddOffer, EditOffer, GetOffers, GetOneOffer
 
 offer = Blueprint('offers', __name__)
-maintainance_api = Api(offer)
-maintainance_api.add_resource(AddOffer, '/api/v1/offers')
-maintainance_api.add_resource(GetOffers, '/api/v1/offers')
-maintainance_api.add_resource(GetOneOffer, '/api/v1/offers/<offer_id>')
-maintainance_api.add_resource(EditOffer, '/api/v1/<offer_id>')
+api = Api(offer)
+api.add_resource(AddOffer, '/api/v1/offers')
+api.add_resource(GetOffers, '/api/v1/offers')
+api.add_resource(GetOneOffer, '/api/v1/offers/<offer_id>')
+api.add_resource(EditOffer, '/api/v1/<offer_id>')
