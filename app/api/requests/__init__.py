b7876dc54a98a1e99db7faa1253c03c30b72f8fd
nakatuddesuzan <nakatuddesusan@gmail.com>
2018-06-30 12:11:21 +0300

    Add changes

--- app/api/requests/__init__.py
+++ app/api/requests/__init__.py
@@ -3,8 +3,8 @@ from flask_restful import Api
 from .views import AddRequest, EditRequest, GetRequests, GetOneRequest
 
 request = Blueprint('requests', __name__)
-maintainance_api = Api(request)
-maintainance_api.add_resource(AddRequest, '/api/v1/requests')
-maintainance_api.add_resource(GetRequests, '/api/v1/requests')
-maintainance_api.add_resource(GetOneRequest, '/api/v1/requests/<request_id>')
-maintainance_api.add_resource(EditRequest, '/api/v1/requests/<request_id>')
+api = Api(request)
+api.add_resource(AddRequest, '/api/v1/requests')
+api.add_resource(GetRequests, '/api/v1/requests')
+api.add_resource(GetOneRequest, '/api/v1/requests/<request_id>')
+api.add_resource(EditRequest, '/api/v1/requests/<request_id>')
